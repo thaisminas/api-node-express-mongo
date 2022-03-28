301e6ff56a7bb8ada1781b6ace910215cf2fbed6
thaisminas <thaisminasds@gmail.com>
2022-03-28 18:32:41 -0300

    removendo informacoes

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 
-mongoose.connect("mongodb+srv://thais2929:yLeI2WJXyHO76ZpD@cluster0.ggebz.mongodb.net/alura-node")
+mongoose.connect("mongodb+srv://thais2929:1234@cluster0.ggebz.mongodb.net/alura-node")
 
 let db = mongoose.connection;
 
